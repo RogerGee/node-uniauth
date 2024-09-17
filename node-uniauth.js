62088cc95d66831d0071d0fbe1c8e2f402022f70
Roger Gee <rpg11a@acu.edu>
2024-09-17 16:53:41 -0500

    Make entrypoint script executable

--- node-uniauth.js
+++ node-uniauth.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
 /**
  * main.js
  *
  * node-uniauth
  */
 
+const path = require("path");
+
 const { execute } = require("./src/main");
-execute(process.argv);
+
+execute(path.basename(process.argv[1]),process.argv);
